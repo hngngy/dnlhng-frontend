406767ab9b9c39596c15a2114dc1d87f30965af8
Hoang Nguyen <hoang_ngy@yahoo.de>
2024-05-28 14:43:56 +0200

    test

--- src/components/PostComponent.vue
+++ src/components/PostComponent.vue
@@ -27,8 +27,7 @@ export default {
   },
   methods: {
     loadPosts() {
-      console.log('blablabla')
-      const baseUrl = import.meta.env.VITE_APP_BACKEND_BASE_URL
+      const baseUrl = 'https://dnlhng-backend.onrender.com'
       const endpoint = baseUrl + '/posts'
       const requestOptions = {
         method: 'GET',
