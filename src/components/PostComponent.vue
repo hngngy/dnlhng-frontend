90cc9a47b59fd96c9b7a8a84413b4ee2dc4579c6
Hoang Nguyen <hoang_ngy@yahoo.de>
2024-06-29 16:58:24 +0200

    delete + create fix

--- src/components/PostComponent.vue
+++ src/components/PostComponent.vue
@@ -48,7 +48,7 @@ export default {
     deletePost(id) {
       axios
         .delete(`${baseUrl}/posts/${id}`)
-        .then(() => (this.posts = this.posts.filter((post) => post.id !== id))) //, this.loadPosts())
+        .then(() => (this.posts = this.posts.filter((post) => post.id !== id)))
         .catch((error) => {
           console.error('Error deleting post:', error)
         })
@@ -60,7 +60,7 @@ export default {
       }
       axios
         .post(`${baseUrl}/posts`, data)
-        .then((response) => this.posts.push(response.data)) //, this.loadPosts())
+        .then((response) => this.posts.push(response.data))
         .catch((error) => console.log(error))
     }
   },
