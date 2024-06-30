ece2e6a0a2187280ee0d765f1fe513d4eae028c9
Hoang Nguyen <hoang_ngy@yahoo.de>
2024-06-30 23:12:42 +0200

    about page text

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,20 +1,14 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <p>
-      Wir haben ein "mini social network" erstellt in dem Nutzer: - über eine Navbar über 3
-      verschiedene Seiten navigieren können - einen Post erstellen können mit Username und
-      Nachricht, wobei die Inputfelder selbst ausgefüllt werden - die erstellten Nachrichten löschen
-      können - die erstellten Usernamen und Nachrichten ändern können - bei einer hohen Anzahl an
-      Posts, über mehrere Seiten navigieren können um ältere Posts zu sehen - über einen Knopf einen
-      Dark Mode togglen können
-    </p>
+    <p>Daniel Nothelfer und Dinh Hoang Nguyen von der HTW Berlin</p>
   </div>
 </template>
 
 <style>
 .about {
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   min-height: calc(100vh - 100px);
